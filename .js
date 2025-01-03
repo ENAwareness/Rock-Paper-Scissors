387e88a96bce5c22dfd674f54c7f2404f3da4056
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-01-04 08:47:13 +0900

    Finishing the step.1 ~ step.4.

--- .js
+++ .js
@@ -1,1 +1,35 @@
-console.log("Hello");
+function humanScore() {
+    let humanScore = 0;
+}
+
+function computerScore() {
+    let computerScore = 0;
+}
+
+function getComputerChoice() {
+    let getRandomNum = Math.random();
+    
+    if (getRandomNum >= 0 && getRandomNum < 1/3) {
+        return ("Rock");
+    } else if (getRandomNum >= 1/3 && getRandomNum< 2/3){
+        return ("Paper");
+    } else  {
+        return("Scissors");
+    }
+  }
+
+console.log(getComputerChoice())
+
+function getHumanChoice() {
+    let aHumanChoice = prompt("Input Rock, Paper or Scissors?", "");
+    aHumanChoice = aHumanChoice.charAt(0).toUpperCase() + aHumanChoice.slice(1);
+
+    if (aHumanChoice == "Rock" || aHumanChoice == "Paper" || aHumanChoice == "Scissors") {
+        return(aHumanChoice)
+    } else {
+        return("rock, paper, scissors only!")
+    }
+}
+
+console.log(getHumanChoice())
+
