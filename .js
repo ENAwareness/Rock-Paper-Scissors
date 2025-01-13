1f529892af6c3bc337cc822fc00fa7135e109265
ENAwareness <166632095+ENAwareness@users.noreply.github.com.>
2025-01-13 21:20:57 +0900

    Complete core logic for Rock Paper Scissors game.

--- .js
+++ .js
@@ -1,8 +1,9 @@
 let playerChoice = "";
 let humanScore = 0;
 let computerScore = 0;
+let isGameOver = false;
 
-function computerChoice() {
+function getComputerChoice() {
   let getRandomNum = Math.random();
 
   if (getRandomNum >= 0 && getRandomNum < 1 / 3) {
@@ -30,36 +31,66 @@ function setupGame() {
     playerChoice = "Scissors";
     playRound();
   });
+  const resetBtn = document.querySelector("#btn-reset");
+  resetBtn.addEventListener("click", () => {
+    resetGame();
+    resetBtn.style.display = "none";
+  });
 }
 
 function updateScore() {
   const displayScore = document.querySelector("#score");
-  displayScore.textContent =
-    `Player score: ${humanScore} | Computer score: ${computerScore}`;
+  displayScore.textContent = `Player score: ${humanScore} | Computer score: ${computerScore}`;
+}
+
+function selectWinner() {
+  const winnerDisplay = document.querySelector("#winner");
+  const resetBtn = document.querySelector("#btn-reset")
+  if (humanScore === 5) {
+    winnerDisplay.textContent = "You are the winner!";
+    isGameOver = true;
+    resetBtn.style.display = "block";
+  }
+  if (computerScore === 5) {
+    winnerDisplay.textContent = "Computer is the winner!";
+    isGameOver = true;
+    resetBtn.style.display = "block";
+  }
 }
 
 function playRound() {
-  const computerPick = computerChoice();
+  if (isGameOver) {
+    return;
+  }
+  const computerPick = getComputerChoice();
+  const roundResult = document.querySelector("#round-result");
 
   console.log("Player choice: ", playerChoice);
   console.log("Computer choice: ", computerPick);
 
   if (playerChoice === computerPick) {
-    const roundResult = document.querySelector("#round-result");
     roundResult.textContent = " Round result: It's a tie!";
   } else if (
     (playerChoice === "Rock" && computerPick === "Scissors") ||
     (playerChoice === "Paper" && computerPick === "Rock") ||
     (playerChoice === "Scissors" && computerPick === "Paper")
   ) {
-    const roundResult = document.querySelector("#round-result");
     roundResult.textContent = " Round result: You win!";
     humanScore++;
   } else {
-    const roundResult = document.querySelector("#round-result");
     roundResult.textContent = " Round result: You lose!";
     computerScore++;
   }
+  updateScore();
+  selectWinner();
+}
+
+function resetGame() {
+  humanScore = 0;
+  computerScore = 0;
+  isGameOver = false;
+  document.querySelector("#round-result").textContent = "Round result: ";
+  document.querySelector("#winner").textContent = "";
   updateScore();
 }
 
